05d2c4de0f67a8f94584eb4544f5d93d0d691f0d
bmcpt <iaansagar@gmail.com>
2022-07-02 11:16:12 +0530

    configurator ui

--- configurator/src/main/java/io/bytebeam/uplink/configurator/MainActivity.kt
+++ configurator/src/main/java/io/bytebeam/uplink/configurator/MainActivity.kt
@@ -1,12 +1,113 @@
 package io.bytebeam.uplink.configurator
 
+import android.app.ActivityManager
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ShareActionProvider
+import android.widget.TextView
+import android.widget.Toast
+import io.bytebeam.uplink.common.Constants
 import io.bytebeam.uplink.configurator.R
+import io.bytebeam.uplink.service.UplinkService
+import org.json.JSONObject
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+const val PICK_AUTH_CONFIG = 1
 
 class MainActivity : AppCompatActivity() {
+    lateinit var statusView: TextView
+    lateinit var statusUpdater: ExecutorService
+    lateinit var selectBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        statusView = findViewById(R.id.status_view)
+        statusUpdater = Executors.newSingleThreadExecutor().also {
+            it.execute {
+                while (true) {
+                    var status = "processing"
+                    if (!serviceRunning()) {
+                        status = "0 clients connected"
+                    } else {
+                        val count = getSharedPreferences(UplinkService.PREFS_NAME, Context.MODE_PRIVATE).getInt(UplinkService.PREFS_CLIENTS_COUNT_KEY, 0)
+                        status = "$count client${if (count == 1) { "" } else { "s" }} connected"
+                    }
+                    runOnUiThread {
+                        statusView.text = status
+                    }
+                    Thread.sleep(3000)
+                }
+            }
+        }
+        selectBtn = findViewById(R.id.select_config_btn)
+        selectBtn.setOnClickListener {
+            selectFile()
+        }
+    }
+
+    override fun onDestroy() {
+        statusUpdater.shutdownNow()
+        super.onDestroy()
+    }
+
+    fun selectFile() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "application/json"
+        }
+
+        startActivityForResult(intent, PICK_AUTH_CONFIG)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            PICK_AUTH_CONFIG -> {
+                if (resultCode == RESULT_OK) {
+                    val uri = data?.data
+                    if (uri != null) {
+                        val inputStream = contentResolver.openInputStream(uri)
+                        val jsonString = inputStream?.bufferedReader()?.use { it.readText() }
+                        if (jsonString == null) {
+                            Toast.makeText(this, "Could not read file", Toast.LENGTH_SHORT).show()
+                            return
+                        }
+                        try {
+                            val json = JSONObject(jsonString)
+                            // TODO: verify properties
+                        } catch (e: Exception) {
+                            Toast.makeText(this, "Invalid JSON", Toast.LENGTH_SHORT).show()
+                            return
+                        }
+                        getSharedPreferences(UplinkService.PREFS_NAME, Context.MODE_PRIVATE).edit().let {
+                            it.putString(UplinkService.PREFS_AUTH_CONFIG_NAME_KEY, uri.toString())
+                            it.putString(UplinkService.PREFS_AUTH_CONFIG_KEY, jsonString)
+                            it.apply()
+                        }
+                        runOnUiThread {
+                            selectBtn.text = uri.toString()
+                            statusView.text = "restarting service"
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fun serviceRunning(): Boolean {
+        val am = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        for (info in am.getRunningServices(Int.MAX_VALUE)) {
+            if (info.service.equals(ComponentName(Constants.CONFIGURATOR_APP_ID, Constants.UPLINK_SERVICE_ID))) {
+                return true
+            }
+        }
+        return false
     }
 }
--- configurator/src/main/java/io/bytebeam/uplink/service/UplinkService.java
+++ configurator/src/main/java/io/bytebeam/uplink/service/UplinkService.java
@@ -2,6 +2,7 @@ package io.bytebeam.uplink.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.*;
 import android.util.Log;
 import androidx.annotation.Nullable;
@@ -16,39 +17,80 @@ import java.util.List;
 import static io.bytebeam.uplink.common.Constants.*;
 
 public class UplinkService extends Service {
-    public static String TAG = "UplinkService";
+    public static final String TAG = "UplinkService";
+    public static final String KILL_COMMAND_TAG = "KILL_SERVICE";
+    public static final String PREFS_NAME = "configuration";
+    public static final String PREFS_AUTH_CONFIG_KEY = "auth_config";
+    public static final String PREFS_AUTH_CONFIG_NAME_KEY = "auth_config_name";
+    public static final String PREFS_CLIENTS_COUNT_KEY = "clients_count";
     List<Messenger> subscribers = new ArrayList<>();
     long uplink = 0;
+
+    int clientsCount = 0;
+    private void setClientsCount(int newCount) {
+        clientsCount = newCount;
+        SharedPreferences.Editor prefs = getApplicationContext().getSharedPreferences(PREFS_NAME, MODE_PRIVATE).edit();
+        prefs.putInt(PREFS_CLIENTS_COUNT_KEY, clientsCount);
+        prefs.apply();
+    }
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        uplink = NativeApi.createUplink(
-                Utils.getRawTextFile(getApplicationContext(), R.raw.auth_config),
-                String.format(
-                        "[persistence]\n" +
-                                "path = \"%s/uplink\"\n" +
-                                "\n" +
-                                "[streams.battery_stream]\n" +
-                                "topic = \"/tenants/{tenant_id}/devices/{device_id}/events/battery_level\"\n" +
-                                "buf_size = 1\n",
-                        getApplicationContext().getFilesDir().getAbsolutePath()
-                ),
-                true,
-                this::uplinkSubscription
-        );
-        Log.d(TAG, "Uplink service created");
-        return new Messenger(new Handler(Looper.myLooper(), this::handleMessage)).getBinder();
+        if (intent.getBooleanExtra(KILL_COMMAND_TAG, false)) {
+            Log.d(TAG, "killing service");
+            onUnbind(null);
+            return null;
+        }
+
+        SharedPreferences prefs = getApplicationContext().getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        String authConfig = prefs.getString(PREFS_AUTH_CONFIG_KEY, null);
+        if (authConfig == null) {
+            // TODO: handle this
+            authConfig = Utils.getRawTextFile(getApplicationContext(), R.raw.auth_config);
+        }
+
+        if (uplink == 0) {
+            uplink = NativeApi.createUplink(
+                    authConfig,
+                    String.format(
+                            "[persistence]\n" +
+                                    "path = \"%s/uplink\"\n" +
+                                    "\n" +
+                                    "[streams.battery_stream]\n" +
+                                    "topic = \"/tenants/{tenant_id}/devices/{device_id}/events/battery_level\"\n" +
+                                    "buf_size = 1\n",
+                            getApplicationContext().getFilesDir().getAbsolutePath()
+                    ),
+                    true,
+                    this::uplinkSubscription
+            );
+            Log.d(TAG, "uplink native context initialized");
+        }
+
+        IBinder result = new Messenger(new Handler(Looper.myLooper(), this::handleMessage)).getBinder();
+
+        try {
+            result.linkToDeath(() -> {
+                Log.d(TAG, "binder died");
+                setClientsCount(clientsCount-1);
+            }, 0);
+        } catch (RemoteException e) {}
+        setClientsCount(clientsCount+1);
+
+        return result;
     }
 
     @Override
     public boolean onUnbind(Intent intent) {
         Log.d(TAG, "shutting down uplink service and process");
         subscribers.clear();
-        NativeApi.destroyUplink(uplink);
-        uplink = 0;
+        if (uplink != 0) {
+            NativeApi.destroyUplink(uplink);
+            uplink = 0;
+        }
         // forcefully kill the service process to allow the cleanup of the native resources
-        Runtime.getRuntime().exit(0);
+        System.exit(0);
         return false;
     }
 
