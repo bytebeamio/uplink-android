2e815310d05b80cabd38777d36eb13397acae8b4
bmcpt <iaansagar@gmail.com>
2023-01-02 16:04:49 +0530

    tweak uplink module

--- module_template/service.sh
+++ module_template/service.sh
@@ -3,5 +3,8 @@
 MODULE_DIR=/data/local/uplinkmodule
 DATA_DIR=/data/local/uplink
 
+pkill -x uplink
+mkdir -p $DATA_DIR
+
 cd $DATA_DIR|| exit
-("$MODULE_DIR"/bin/uplink -a $DATA_DIR/device.json -c "$MODULE_DIR"/etc/uplink.config.toml -v 2>&1 | "$MODULE_DIR"/bin/logrotate $DATA_DIR/out.log) &
+("$MODULE_DIR"/bin/uplink -a $DATA_DIR/device.json -c "$MODULE_DIR"/etc/uplink.config.toml -v 2>&1 | "$MODULE_DIR"/bin/logrotate --output $DATA_DIR/out.log) &
