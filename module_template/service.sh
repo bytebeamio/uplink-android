615541aba2797f0e8f09ef12b3c42d05cfca76f8
bmcpt <iaansagar@gmail.com>
2023-01-13 13:38:24 +0530

    verbose logs

--- module_template/service.sh
+++ module_template/service.sh
@@ -8,5 +8,5 @@ pkill -x uplink
 mkdir -p $DATA_DIR
 
 cd $DATA_DIR|| exit
-("$MODULE_DIR"/bin/uplink -a $DATA_DIR/device.json -c "$MODULE_DIR"/etc/uplink.config.toml -v 2>&1 | "$MODULE_DIR"/bin/logrotate --output $DATA_DIR/out.log) &
+("$MODULE_DIR"/bin/uplink -a $DATA_DIR/device.json -c "$MODULE_DIR"/etc/uplink.config.toml -vv 2>&1 | "$MODULE_DIR"/bin/logrotate --output $DATA_DIR/out.log) &
 "$MODULE_DIR"/bin/uplink_watchdog >> $DATA_DIR/restart.log 2>&1
