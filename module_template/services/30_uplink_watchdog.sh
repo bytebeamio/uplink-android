f8640b4e5e56cba24daad394391298414ac4f140
amokfa <iaansagar@gmail.com>
2023-04-07 11:48:24 +0530

    make uplink_watchdog use logrotate

--- module_template/services/30_uplink_watchdog.sh
+++ module_template/services/30_uplink_watchdog.sh
@@ -1,3 +1,3 @@
 #!/system/bin/sh
 
-$MODULE_DIR/bin/daemonize sh -c "$MODULE_DIR/bin/uplink_watchdog >> $DATA_DIR/restart.log 2>&1"
+$MODULE_DIR/bin/daemonize sh -c "$MODULE_DIR/bin/uplink_watchdog 2>&1 | $MODULE_DIR/bin/logrotate --max-size 1024000 --backup-files-count 1 --output $DATA_DIR/restart.log"
