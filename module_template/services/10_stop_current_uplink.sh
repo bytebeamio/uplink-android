6f59b454ff4b1c222e5b595d86463e25c1fbfbb0
amokfa <iaansagar@gmail.com>
2023-08-25 09:59:38 +0530

    wait for uplink to stop when killing it

--- module_template/services/10_stop_current_uplink.sh
+++ module_template/services/10_stop_current_uplink.sh
@@ -2,3 +2,23 @@
 
 pkill -x uplink_watchdog
 pkill -x uplink
+
+# check for 10s if uplink has stopped
+# do a pkill -9 -x uplink if it's still running
+
+KILLED=false
+for i in $(seq 1 10); do
+  if ! pgrep -x uplink; then
+    echo uplink has stopped
+    KILLED=true
+    break
+  fi
+  sleep 1
+done
+
+if [ $KILLED = false ]; then
+  echo uplink still running, sending sigkill
+  pkill -9 -x uplink
+fi
+
+
